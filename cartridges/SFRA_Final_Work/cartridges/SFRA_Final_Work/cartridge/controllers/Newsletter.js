c5bf8bcebc6f5a2cc7227eca92437142259d5545
PauloProenca <paulojorgeproenca@gmail.com>
2022-06-05 22:18:31 +0100

    first commit

---
+++ cartridges/SFRA_Final_Work/cartridges/SFRA_Final_Work/cartridge/controllers/Newsletter.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var server = require('server');
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var URLUtils = require('dw/web/URLUtils');
+
+server.get(
+    'Show',
+    server.middleware.https,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var actionUrl = dw.web.URLUtils.url('Newsletter-Handler');
+        var newsletterForm = server.forms.getForm('newsletter');
+        newsletterForm.clear();
+
+        res.render('/newsletter/newslettersignup', {
+            actionUrl: actionUrl,
+            newsletterForm: newsletterForm
+        });
+
+        next();
+    }
+);
+
+server.post(
+    'Handler',
+    server.middleware.https,csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+    	var newsletterForm = server.forms.getForm('newsletter');
+    	var continueUrl = URLUtils.url('Newsletter-Show');
+
+
+    	if (newsletterForm.valid) {
+            var Transaction = require('dw/system/Transaction');
+    		try {
+                Transaction.wrap(function(){
+                    var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+                    var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
+                    co.custom.firstName = newsletterForm.fname.value;
+                    co.custom.lastName = newsletterForm.lname.value
+                    res.render('/newsletter/newslettersuccess',{
+                        continueUrl: continueUrl,
+                        newsletterForm: newsletterForm
+                    });
+                });
+            }catch(e){
+                var err = e;
+                res.render('/newsletter/newslettererror', {
+                    errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
+                    continueUrl: continueUrl
+                });
+            }
+        }else{
+            res.render('/newsletter/newslettererror', {
+            errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
+            continueUrl: continueUrl
+            });
+        }
+        next();
+    }
+);
+
+
+
+module.exports = server.exports();
